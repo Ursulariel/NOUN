47da0758d078a1e870abc1e6558ff3be2bbfd4da
ursula.silva <ursula.silva@ambevtech.com.br>
2021-10-11 18:11:56 -0300

    NOUN

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import PrincipalPage from './PrincipalPage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PrincipalPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
